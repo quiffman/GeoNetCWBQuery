1df5a0d9f4be79fc8f5685a02eebca4deb60a3c4
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2009-07-28 02:49:29 +0000

    Added toHex methods from usgs Util class so can remove dependency on that.

--- src/gov/usgs/anss/query/Outputer.java
+++ src/gov/usgs/anss/query/Outputer.java
@@ -31,4 +31,60 @@ abstract public class Outputer {
      */
     abstract public void makeFile(String comp, String filename, String mask, ArrayList<MiniSeed> blks,
             java.util.Date beg, double duration, String[] args) throws IOException;
+
+      /** convert to hex string
+     *@param b The item to convert to hex
+     *@return The hex string */
+    protected static String toHex(byte b) {
+        return toHex(((long) b) & 0xFFL);
+    }
+
+    /** convert to hex string
+     *@param b The item to convert to hex
+     *@return The hex string */
+    protected static String toHex(short b) {
+        return toHex(((long) b) & 0xFFFFL);
+    }
+
+    /** convert to hex string
+     *@param b The item to convert to hex
+     *@return The hex string */
+    protected static String toHex(int b) {
+        return toHex(((long) b) & 0xFFFFFFFFL);
+    }
+
+    /** convert to hex string
+     *@param i The item to convert to hex
+     *@return The hex string */
+    protected static String toHex(long i) {
+        StringBuilder s = new StringBuilder(16);
+        int j = 60;
+        int k;
+        long val;
+        char c;
+        boolean flag = false;
+        s.append("0x");
+
+        for (k = 0; k < 16; k++) {
+            val = (i >> j) & 0xf;
+            //prt(i+" i >> j="+j+" 0xF="+val);
+            if (val < 10) {
+                c = (char) (val + '0');
+            } else {
+                c = (char) (val - 10 + 'a');
+            }
+            if (c != '0') {
+                flag = true;
+            }
+            if (flag) {
+                s.append(c);
+            }
+            j = j - 4;
+        }
+        if (!flag) {
+            s.append("0");
+        }
+        return s.toString();
+    }
+
 }

6f2032ae170ecfe49ca8677645e225cf130ba66a
Richard Guest <r.guest@gns.cri.nz>
2010-01-19 22:48:45 +0000

    Adding a "SeedName" object. This Encapsulates Network, Station, Channel and Location Strings and (ultimately) provides parser methods for different concatenated NSCL String formats.

---
+++ src/gov/usgs/anss/query/SeedName.java
@@ -0,0 +1,124 @@
+package gov.usgs.anss.query;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author richardg
+ */
+public class SeedName {
+	private String network,station,channel,location;
+	
+	public enum Format {
+		NSCL(Pattern.compile("([A-Z]{2})([A-Z ]{5})([A-Z]{3})([0-9]{2})")),
+		SCNL(Pattern.compile("")),
+		SLCN(Pattern.compile(""));
+		
+		private Pattern pattern;
+
+		Format(Pattern pattern) {
+			this.pattern = pattern;
+		}
+
+		public String[] split(String input) {
+			Matcher m = pattern.matcher(input);
+			return new String[]{m.group(1), m.group(2), m.group(3), m.group(4)};
+		}
+	}
+
+	public SeedName(String network, String station, String channel, String location) {
+		this.network = network;
+		this.station = station;
+		this.channel = channel;
+		this.location = location;
+	}
+
+	//TODO throw IllegalArgumentException...?
+	public static SeedName stringToSeedName(SeedName.Format format, String input) {
+		String[] e = format.split(input);
+		
+		return new SeedName(e[0],e[1],e[2],e[3]);
+	}
+
+	/**
+	 * Returns a new SeedName object constructed from the NNSSSSSCCCLL input String.
+	 * @param input 12 character String formatted as NNSSSSSCCLL.
+	 * @return a new SeedName representation of the input NSCL.
+	 */
+	public static SeedName nsclStringToSeedName(String input) {
+		return new SeedName(input.substring(0, 2), input.substring(2, 7),
+				input.substring(7, 10), input.substring(10, 12));
+	}
+
+	/**
+	 * @return the network
+	 */
+	public String getNetwork() {
+		return network;
+	}
+
+	/**
+	 * @param network the network to set
+	 */
+	public void setNetwork(String network) {
+		this.network = network;
+	}
+
+	/**
+	 * @return the station
+	 */
+	public String getStation() {
+		return station;
+	}
+
+	/**
+	 * @param station the station to set
+	 */
+	public void setStation(String station) {
+		this.station = station;
+	}
+
+	/**
+	 * @return the channel
+	 */
+	public String getChannel() {
+		return channel;
+	}
+
+	/**
+	 * @param channel the channel to set
+	 */
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		SeedName other = (SeedName) obj;
+		return this.network.equals(other.network) &&
+				this.station.equals(other.station) &&
+				this.channel.equals(other.channel) &&
+				this.location.equals(other.location);
+	}
+
+	@Override
+	public String toString() {
+		return (network + station + channel + location);
+	}
+
+}
---
+++ test/gov/usgs/anss/query/SeedNameTest.java
@@ -0,0 +1,194 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.usgs.anss.query;
+
+import gov.usgs.anss.query.SeedName.Format;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author richardg
+ */
+public class SeedNameTest {
+
+    public SeedNameTest() {
+    }
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	/**
+	 * Test of stringToSeedName method, of class SeedName.
+	 */
+	@Test
+	public void testStringToSeedName() {
+		System.out.println("stringToSeedName");
+		String input = "NZWEL  BHZ10";
+		String n="NZ", s="WEL  ", c="BHZ", l="10";
+		SeedName expResult = new SeedName(n,s,c,l);
+		SeedName result = SeedName.stringToSeedName(SeedName.Format.NSCL, input);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of getNetwork method, of class SeedName.
+	 */
+	@Test
+	public void testGetNetwork() {
+		System.out.println("getNetwork");
+		SeedName instance = null;
+		String expResult = "";
+		String result = instance.getNetwork();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of setNetwork method, of class SeedName.
+	 */
+	@Test
+	public void testSetNetwork() {
+		System.out.println("setNetwork");
+		String network = "";
+		SeedName instance = null;
+		instance.setNetwork(network);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of getStation method, of class SeedName.
+	 */
+	@Test
+	public void testGetStation() {
+		System.out.println("getStation");
+		SeedName instance = null;
+		String expResult = "";
+		String result = instance.getStation();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of setStation method, of class SeedName.
+	 */
+	@Test
+	public void testSetStation() {
+		System.out.println("setStation");
+		String station = "";
+		SeedName instance = null;
+		instance.setStation(station);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of getChannel method, of class SeedName.
+	 */
+	@Test
+	public void testGetChannel() {
+		System.out.println("getChannel");
+		SeedName instance = null;
+		String expResult = "";
+		String result = instance.getChannel();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of setChannel method, of class SeedName.
+	 */
+	@Test
+	public void testSetChannel() {
+		System.out.println("setChannel");
+		String channel = "";
+		SeedName instance = null;
+		instance.setChannel(channel);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of getLocation method, of class SeedName.
+	 */
+	@Test
+	public void testGetLocation() {
+		System.out.println("getLocation");
+		SeedName instance = null;
+		String expResult = "";
+		String result = instance.getLocation();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of setLocation method, of class SeedName.
+	 */
+	@Test
+	public void testSetLocation() {
+		System.out.println("setLocation");
+		String location = "";
+		SeedName instance = null;
+		instance.setLocation(location);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of nsclStringToSeedName method, of class SeedName.
+	 */
+	@Test
+	public void testNsclStringToSeedName() {
+		System.out.println("nsclStringToSeedName");
+		String input = "NZWEL  BHZ10";
+		String n="NZ", s="WEL  ", c="BHZ", l="10";
+		SeedName expResult = new SeedName(n,s,c,l);
+		SeedName result = SeedName.nsclStringToSeedName(input);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of equals method, of class SeedName.
+	 */
+	@Test
+	public void testEquals() {
+		System.out.println("equals");
+		Object obj = null;
+		SeedName instance = null;
+		boolean expResult = false;
+		boolean result = instance.equals(obj);
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of toString method, of class SeedName.
+	 */
+	@Test
+	public void testToString() {
+		System.out.println("toString");
+		SeedName instance = null;
+		String expResult = "";
+		String result = instance.toString();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+}

dd44aedb11257b820a3081941a31908d1ce97a33
Richard Guest <r.guest@gns.cri.nz>
2010-01-29 01:10:18 +0000

    Refactor as a parameterized test for ease of reuse.

--- test/gov/usgs/anss/query/EdgeQueryClientTest.java
+++ test/gov/usgs/anss/query/EdgeQueryClientTest.java
@@ -9,28 +9,37 @@ import gov.usgs.anss.seed.MiniSeed;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import static org.junit.Assert.*;
 
 /**
  *
  * @author richardg
  */
+@RunWith(Parameterized.class)
 public class EdgeQueryClientTest {
 
-    public EdgeQueryClientTest() {
+    @Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{
+			// args line,	filenames...
+			// Note the -t NULL
+			{"-b \"2009/01/01 00:00:00\" -s \"NZWLGT.BTZ40\" -d 600 -t NULL", new String[] {"test/NZWLGT_BTZ40-09-01-01-00-00-00.ms"}},
+		});
     }
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
+	private EdgeQueryOptions options;
+	private String[] filenames;
 
-	@AfterClass
-	public static void tearDownClass() throws Exception {
+    public EdgeQueryClientTest(String queryLine, String[] filenames) {
+        this.options = new EdgeQueryOptions(queryLine);
+		this.filenames = filenames;
 	}
 
 	/**
@@ -43,22 +52,22 @@ public class EdgeQueryClientTest {
 	@Test
 	public void testQuery() throws Exception {
 		System.out.println("query");
-		// Note the -t NULL
-		EdgeQueryOptions options = new EdgeQueryOptions("-b \"2009/01/01 00:00:00\" -s \"NZWLGT.BTZ40\" -d 600 -t NULL");
 		
 		// Load the blocks
 		ArrayList<ArrayList<MiniSeed>> expResult = new ArrayList<ArrayList<MiniSeed>>();
-		File ms = new File("test/NZWLGT_BTZ40-09-01-01-00-00-00.ms");
-		long fileSize = ms.length();
-		ArrayList<MiniSeed> blks = new ArrayList<MiniSeed>((int) (fileSize / 512));
-		
-		byte[] buf = new byte[512];
-		FileInputStream in = new FileInputStream(ms);
-		for (long pos = 0; pos < fileSize; pos += 512) {
-			if (in.read(buf) == -1) break;
-			blks.add(new MiniSeed(buf));
+		for(String filename : filenames) {
+			File ms = new File(filename);
+			long fileSize = ms.length();
+			ArrayList<MiniSeed> blks = new ArrayList<MiniSeed>((int) (fileSize / 512));
+
+			byte[] buf = new byte[512];
+			FileInputStream in = new FileInputStream(ms);
+			for (long pos = 0; pos < fileSize; pos += 512) {
+				if (in.read(buf) == -1) break;
+				blks.add(new MiniSeed(buf));
+			}
+			expResult.add(blks);
 		}
-		expResult.add(blks);
 		
 		// Run the query
 		ArrayList<ArrayList<MiniSeed>> result = EdgeQueryClient.query(options);

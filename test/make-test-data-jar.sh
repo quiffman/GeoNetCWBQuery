3e885dfb20cbcce741a22ce0e6725a8eee212c05
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2010-02-12 01:45:37 +0000

    Made sure the sac data actually got included in the jar....
    Made the data request very specific to get the jar size down.

--- test/make-test-data-jar.sh
+++ test/make-test-data-jar.sh
@@ -9,19 +9,19 @@ cp make-test-data-jar.sh $tmpdir
 
 mkdir -p $tmpdir/miniseed-data/test-one
 
-echo 'java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t msz -b "2009/01/01 00:00:00" -d 1800 -s "NZ.....HH.10" -o $tmpdir/miniseed-data/test-one/%N.ms' > $tmpdir/miniseed-data/test-one/command.txt
+echo 'java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t msz -b "2009/01/01 00:00:00" -d 1800 -s "NZMRZ..HH.10" -o $tmpdir/miniseed-data/test-one/%N.ms' > $tmpdir/miniseed-data/test-one/command.txt
 
-java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t msz -b "2009/01/01 00:00:00" -d 1800 -s "NZ.....HH.10" -o $tmpdir/miniseed-data/test-one/%N.ms
+java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t msz -b "2009/01/01 00:00:00" -d 1800 -s "NZMRZ..HH.10" -o $tmpdir/miniseed-data/test-one/%N.ms
 
 mkdir -p $tmpdir/sac-data/test-one
 
-echo 'java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t sac -sacpz nm -b "2009/01/01 00:00:00" -d 1800 -s "NZ.....HH.10" -o $tmpdir/sac-data/test-one/%N.ms' > $tmpdir/sac-data/test-one/command.txt
+echo 'java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t sac -sacpz nm -b "2009/01/01 00:00:00" -d 1800 -s "NZMRZ..HH.10" -o $tmpdir/sac-data/test-one/%N.ms' > $tmpdir/sac-data/test-one/command.txt
 
-java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t sac -sacpz nm -b "2009/01/01 00:00:00" -d 1800 -s "NZ.....HH.10" -o $tmpdir/sac-data/test-one/%N.sac
+java -jar ../lib-ivy/external/GeoNetCWBQuery-2.0.0-RC1.jar -t sac -sacpz nm -b "2009/01/01 00:00:00" -d 1800 -s "NZMRZ..HH.10" -o $tmpdir/sac-data/test-one/%N.sac
 
 cd $tmpdir
 
-jar -0cvf CWBQueryTestData-${jarversion}.jar miniseed-data make-test-data-jar.sh
+jar -0cvf CWBQueryTestData-${jarversion}.jar miniseed-data sac-data  make-test-data-jar.sh
 
 cd -
 
